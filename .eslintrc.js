7f2817104391f727c1f1962c329f3a734c6ae136
p060651 <diego.ferreira@prodemge.gov.br>
2019-07-24 10:27:48 -0300

    Ajuste conforme tutorial inicial de estudo.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   extends: ["airbnb-base", "prettier"],
   plugins: ["prettier"],
   globals: {
-    Atomics: "readable",
-    SharedArrayBuffer: "readable"
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018,
@@ -17,7 +17,7 @@ module.exports = {
     "prettier/prettier": "error",
     "class-methods-use-this": "off", //com esse parametro nao é necessario usar o this dentro dos metodos das classes
     "no-param-reassign": "off", // permite manipular o valor do parametro de um metodo
-    camelcase: "off", //permitir variavel com "_" ex.: numero_endereco senao eslint apenas aceita variavel numeroEndereco
+    "camelcase": "off", //permitir variavel com "_" ex.: numero_endereco senao eslint apenas aceita variavel numeroEndereco
     "no-unused-vars": ["error", { argsIgnorePattern: ["next", "Sequelize"] }] //exibe msg erro variaveis nao usadas, como esse comando ele ignora as que estao no parametro
   }
 };
