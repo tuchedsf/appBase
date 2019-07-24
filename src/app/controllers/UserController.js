a059ee39c625d7cd264a395176bc5a4386ca1cf3
p060651 <diego.ferreira@prodemge.gov.br>
2019-07-24 09:56:10 -0300

    ajuste formatação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,51 +2,51 @@ import User from '../models/User';
 class UserController { 
 
     async index(req, res) {
-        const users = await User.findAll();
-        return res.json(users)
+      const users = await User.findAll();
+      return res.json(users)
     }
 
     async store(req,res) {
-        try{
-            const user = await User.findOne({ where: { email: req.body.email }});
+      try{
+        const user = await User.findOne({ where: { email: req.body.email }});
             
-            if (user) return res.status(400).json({message: "E-mail já cadastrado!!"});
+        if (user) return res.status(400).json({message: "E-mail já cadastrado!!"});
 
-            const {id, name, email } = await User.create(req.body);
-            return res.json({ id , name, email });
+        const {id, name, email } = await User.create(req.body);
+        return res.json({ id , name, email });
 
-        }catch(error){
-            return res.status(400).json({message: error})
-        }
+      }catch(error){
+        return res.status(400).json({message: error})
+      }
 
     } 
 
     async update(req, res) {
         
-        const { email, oldPassword } = req.body;
+      const { email, oldPassword } = req.body;
         
-        const userExists = await User.findByPk(req.userId);
+      const userExists = await User.findByPk(req.userId);
     
-        if (email && email !== userExists.email) {
-          const userExists = await User.findOne({ where: { email } });
-          if (userExists) {
-            res.status(400).json({ error: 'USUARIO JA CADASTRADO' });
-          }
+      if (email && email !== userExists.email) {
+        const userExists = await User.findOne({ where: { email } });
+        if (userExists) {
+          res.status(400).json({ error: 'USUARIO JA CADASTRADO' });
         }
-    
-        if (oldPassword && !(await userExists.verifyPassword(oldPassword))) {
-          res.status(400).json({ error: 'OLD PASSWORD INVALIDO' });
-        }
-    
-        const { id, name, provider } = await userExists.update(req.body);
-    
-        return res.json({
-          id,
-          name,
-          email,
-          provider,
-        });
       }
+    
+      if (oldPassword && !(await userExists.verifyPassword(oldPassword))) {
+        res.status(400).json({ error: 'OLD PASSWORD INVALIDO' });
+      }
+    
+      const { id, name, provider } = await userExists.update(req.body);
+    
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
+    }
 
 }
 
